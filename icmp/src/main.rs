97313b47811c0d70d607db2d2ffc80c70905ff5c
noreplydev <contact.cristiansanchez@gmail.com>
2023-09-06 01:21:40 +0200

    [FT](ICMP) Created first raw socket

--- icmp/src/main.rs
+++ icmp/src/main.rs
@@ -1,3 +1,13 @@
+use socket2::{Domain, Socket};
+
 fn main() {
     // again, start from 0. I can't achieve an icmp implementation
+    let raw_socket = Socket::new(
+        Domain::IPV4,
+        socket2::Type::RAW,
+        Some(socket2::Protocol::ICMPV4),
+    )
+    .unwrap();
+
+    println!("Hello, world!");
 }
